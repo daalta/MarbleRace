2fe151baabaac21055a9775deafefb452eaea093
Fax <dtauritis@gmail.com>
2022-03-01 00:07:08 +0100

    Spinning obstacles

---
+++ Assets/MarbleRace/Scripts/SpinObstacle.cs
@@ -0,0 +1,45 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace MarbleRace.Scripts
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual), RequireComponent(typeof(Rigidbody2D))]
+    public class SpinObstacle : UdonSharpBehaviour
+    {
+        [SerializeField] private float spinSpeed = 1f;
+
+        [UdonSynced] private float syncedRotation;
+        
+        private Rigidbody2D rigidbody2d;
+        
+        void Start()
+        {
+            rigidbody2d = GetComponent<Rigidbody2D>();
+            rigidbody2d.angularVelocity = -spinSpeed;
+            transform.rotation = Quaternion.identity;
+            _SyncRotation();
+        }
+
+        public override void OnDeserialization()
+        {
+            _SyncRotation();
+        }
+
+        public void _SyncRotation()
+        {
+            if (rigidbody2d == null) return;
+            if (Networking.IsMaster)
+            {
+                syncedRotation = rigidbody2d.rotation;
+                RequestSerialization();
+            }
+            else
+            {
+                rigidbody2d.SetRotation(syncedRotation);
+            }
+            
+            SendCustomEventDelayedSeconds(nameof(_SyncRotation), UnityEngine.Random.Range(10f, 30f));
+        }
+    }
+}
