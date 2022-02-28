30732afc4811d919d150a670b7160a48f7fa87fe
Fax <dtauritis@gmail.com>
2022-02-28 19:48:23 +0100

    Override Physics 2D velocity threshold

---
+++ Assets/MarbleRace/Scripts/Physics2DSettings.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace MarbleRace.Scripts
+{
+    /// <summary>
+    /// Overrides some Physics 2D settings.
+    /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class Physics2DSettings : UdonSharpBehaviour
+    {
+        [Header("VRChat ignores Physics 2D project settings. This script will allow you to set them anyway..")]
+        [SerializeField] private float velocityThreshold = 0.15f;
+    
+        void Start()
+        {
+            Physics2D.velocityThreshold = velocityThreshold;
+        }
+    }
+}
