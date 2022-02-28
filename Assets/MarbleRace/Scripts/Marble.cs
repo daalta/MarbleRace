d039ec874824273aeca8d8c2351e8fa470fa9d3b
Fax <dtauritis@gmail.com>
2022-02-28 20:02:26 +0100

    Marbles now use custom, manual rigidbody position and velocity sync

---
+++ Assets/MarbleRace/Scripts/Marble.cs
@@ -0,0 +1,38 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace MarbleRace.Scripts
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class Marble : UdonSharpBehaviour
+    {
+        [SerializeField] private new Rigidbody2D rigidbody;
+        [UdonSynced] private Vector2 position;
+        [UdonSynced] private Vector2 velocity;
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            SendRigidbody();
+        }
+
+        private void SendRigidbody()
+        {
+            if (!Networking.IsMaster)
+            {
+                Debug.LogWarning("Marble race: Non-master tried to send rigidbody variables.");
+            }
+
+            position = rigidbody.position;
+            velocity = rigidbody.velocity;
+            RequestSerialization();
+        }
+
+        public override void OnDeserialization()
+        {
+            if (Networking.IsMaster) return; // Master already has the values
+            rigidbody.position = position;
+            rigidbody.velocity = velocity;
+        }
+    }
+}
