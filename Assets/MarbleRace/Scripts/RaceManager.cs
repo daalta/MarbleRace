d972f7dd178c2cffede79f0ee94c61deeb0b032f
Fax <dtauritis@gmail.com>
2022-03-02 17:09:38 +0100

    Changed payouts to 3,2,1,-1

--- Assets/MarbleRace/Scripts/RaceManager.cs
+++ Assets/MarbleRace/Scripts/RaceManager.cs
@@ -154,10 +154,10 @@ namespace MarbleRace.Scripts
         {
             switch (placement)
             {
-                case 0: return 4;
-                case 1: return 3;
-                case 2: return 2;
-                default: return 0;
+                case 0: return 3;
+                case 1: return 2;
+                case 2: return 1;
+                default: return -1;
             }
         }
 
